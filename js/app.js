361886251ce0766441ea222d154e12602f7d0f71
jonnyjava.net@gmail.com <jonnyjava.net@gmail.com>
2015-10-12 17:05:21 +0200

    getting garages json from external API

--- js/app.js
+++ js/app.js
@@ -1,54 +1,17 @@
 (function(){
   var app = angular.module('garages', ['ngMap']); app.js
 
-  app.controller('GaragesController', function(){
-    this.mechanics = garages;
-  });
+  app.controller('GaragesController', ['$http',function($http){
+      var garages = this;
+      garages.mechanics = [];
 
-  var garages = [
-    {
-      "id": 13,
-      "name": "Garage Spain 0",
-      "latitude": "39.455277",
-      "longitude": "-0.358695",
-      "street": "Spain road",
-      "zip": "46000",
-      "province": "Valencia",
-      "city": "Valencia",
-      "email": "fake_ 0_Spain@email.com",
-      "phone": "666 666 666",
-      "mobile": null,
-      "fax": null,
-      "website": null,
-      "logo": "/assets/avatar_default.jpg",
-      "tyre_fees": [
-      ],
-      "timetable": {
+      $http.get('https://stormy-cliffs-9773.herokuapp.com/api/v1/garages.json', {
+        headers: {
+            "Authorization": 'Token token="310b9e31ff8d0fa4ebca7ab1257ca729"'
+        }
+      }).success(function(response){
+        garages.mechanics = response;
+      });
+    }]);
 
-      }
-    },
-    {
-      "id": 15,
-      "name": "Garage Spain 2",
-      "latitude": "39.455277",
-      "longitude": "-0.358695",
-      "street": "Spain road",
-      "zip": "46002",
-      "province": "Valencia",
-      "city": "Valencia",
-      "email": "fake_ 2_Spain@email.com",
-      "phone": "666 666 666",
-      "mobile": null,
-      "fax": null,
-      "website": null,
-      "logo": "/assets/avatar_default.jpg",
-      "tyre_fees": [
-      ],
-      "timetable": {
-
-      }
-    }
-  ]
 })();
-
-//curl -H "Authorization: Token token=6941dbbd72663f8e26ae990c56bf980f" http://0.0.0.0:3000/api/v1/garages.json
