61f19891f09537d481ff5e66530ce93405799db1
jonnyjava.net@gmail.com <jonnyjava.net@gmail.com>
2015-12-17 23:38:50 +0100

    Responsive tab panel for confirmation page steps

--- 123Onboarding.php
+++ 123Onboarding.php
@@ -261,6 +261,7 @@
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/123breadcrumbs.js" type="text/javascript"></script>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/123dropdowns.js" type="text/javascript"></script>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/123services_fields.js" type="text/javascript"></script>
+  <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/123summary.js" type="text/javascript"></script>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/123car_selector.js" type="text/javascript"></script>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/123cars.js" type="text/javascript"></script>
 </div>
--- partials/_confirmation_page.php
+++ partials/_confirmation_page.php
@@ -5,70 +5,109 @@
 </div>
 <div class="row text-left">
   <div class="col-lg-12 confirmation_page">
-    <ul class="list-group">
-      <li class="list-group-item dashed_line">
-        <p>
-          Hemos recibido tu demanda y está siendo examinada por nuestros expertos, gracias por tu confianza!<br/>Pronto recibirás un email con el precio del servicio que has pedido. Recuerda de comprobar tu carpeta de spam etc etc
-        </p>
+    <div class="panel panel-default">
+      <div class="panel-body">
+        Hemos recibido tu demanda y está siendo examinada por nuestros expertos, gracias por tu confianza!<br/>
+        Aquí te detallamos cual son los próximos pasos :
+        <ol>
+          <li>Hemos elegido los mejores talleres por tu demanda en tu zona y  los hemos mandado tu demanda</li>
+          <li>Estos talleres van a estudiar tu demanda</li>
+          <li>Dentro de 5 días, vas a recibir emails de estos talleres con sus presupuestos (Recuérdate de comprobar tu carpeta de spam)</li>
+          <li>A recibir, puedes elegir el taller que prefiera, y después de haber elegido uno, debes llamarlo para definir una fecha de intervención.</li>
+          <li>Durante la intervención, pagaras directamente el taller por su trabajo.</li>
+        </ol>
+        Si tienes alguna duda, contactanos a esta dirección: contact@123mecanico.es, te contestáramos muy rápidamente.<br/>
+        También puedes visitar nuestra página web aquí: http://123mecanico.es/como-funciona/ para tener respuestas a tus dudas.
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-lg-12 confirmation_page text-left">
+    <ul class="nav nav-tabs">
+      <li class="active confirmation_header">
+        <a data-toggle="tab" class="dropdown-toggle" id="step1-tab" href="#step1">
+          <span class="success">
+            <span>1</span>
+          </span>
+          <h4>Tu demanda</h4>
+        </a>
       </li>
-      <li class="list-group-item">
-        <span class="success">
-          <span>1</span>
-        </span>
-        <h4>Tu demanda</h4>
+      <li class="confirmation_header">
+        <a data-toggle="tab" class="dropdown-toggle" id="step2-tab" href="#step2">
+          <span class="success"><span>2</span></span>
+          <h4>Tu coche</h4>
+        </a>
       </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">Servicio:</span>
-        <span class="horizontal_list_content">iluminación y electricidad</span>
-      </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">Servicio:</span>
-        <span class="horizontal_list_content">cambio de cierre centralizado</span>
-      </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">La otra cosa dinamica:</span>
-        <span class="horizontal_list_content">el valor más largo que se pueda</span>
-      </li>
-      <li class="list-group-item dashed_line">
-        <span class="horizontal_list_header large-view">Comentarios adicionales:</span>
-        <span class="horizontal_list_content large-view"><span class="comments">Two queen beds. Dining area with table and chairs. Walk-in closet. Wireless Internet access (surcharge). Satellite TV. Phone with voice mail. Kitchenette with refrigerator, two-burner stove, tableware, and cooking utensils. Private bathroom with hair dryer and shower/tub combination. Iron/ironing board available at the front desk. Air conditioning. Nonsmoking.</span></span>
-      </li>
-      <li class="list-group-item">
-        <span class="success"><span>2</span></span>
-        <h4>Tu coche</h4>
-      </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">Marca:</span>
-        <span class="horizontal_list_content"> VOLKSWAGEN</span>
-      </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">Modelo:</span>
-        <span class="horizontal_list_content"> CRAFTER 30-50 Camionnette</span>
-      </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">Periodo:</span>
-        <span class="horizontal_list_content"> DEPUIS 04-2006</span>
-      </li>
-      <li class="list-group-item dashed_line">
-        <span class="horizontal_list_header">Motor:</span>
-        <span class="horizontal_list_content"> salamadonna ABSDFS</span>
-      </li>
-      <li class="list-group-item">
-        <span class="success"><span>3</span></span>
-        <h4>Tus datos de contacto</h4>
-      </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">Nombre y apellidos:</span>
-        <span class="horizontal_list_content">Poldo de poldis ramirez hernandez de la fuente torres</span>
-      </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">Telefono:</span>
-        <span class="horizontal_list_content">654654654</span>
-      </li>
-      <li class="list-group-item">
-        <span class="horizontal_list_header">Email:</span>
-        <span class="horizontal_list_content">thelargestemailintheworld@superlargedomain.com </span>
+      <li class="confirmation_header">
+        <a data-toggle="tab" class="dropdown-toggle" id="step3-tab" href="#step3">
+          <span class="success"><span>3</span></span>
+          <h4>Tus datos</h4>
+        </a>
       </li>
     </ul>
+  </div>
+</div>
+<div class="row text-left">
+  <div class="col-lg-12 confirmation_page">
+    <div class="tab-content">
+      <div id="step1" class="tab-pane fade active in">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <span class="horizontal_list_header">Categoria:</span>
+            <span class="horizontal_list_content">Iluminación y electricidad</span>
+          </li>
+          <li class="list-group-item">
+            <span class="horizontal_list_header">Servicio:</span>
+            <span class="horizontal_list_content">Cambio de cierre centralizado</span>
+          </li>
+          <li class="list-group-item">
+            <span class="horizontal_list_header">La otra cosa dinamica:</span>
+            <span class="horizontal_list_content">El valor más largo que se pueda</span>
+          </li>
+          <li class="list-group-item">
+            <span class="horizontal_list_header large-view">Comentarios adicionales:</span>
+            <span class="horizontal_list_content large-view"><span class="comments">Two queen beds. Dining area with table and chairs. Walk-in closet. Wireless Internet access (surcharge). Satellite TV. Phone with voice mail. Kitchenette with refrigerator, two-burner stove, tableware, and cooking utensils. Private bathroom with hair dryer and shower/tub combination. Iron/ironing board available at the front desk. Air conditioning. Nonsmoking.</span></span>
+          </li>
+        </ul>
+      </div>
+      <div id="step2" class="tab-pane fade">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <span class="horizontal_list_header">Marca:</span>
+            <span class="horizontal_list_content"> VOLKSWAGEN</span>
+          </li>
+          <li  class="list-group-item">
+            <span class="horizontal_list_header">Modelo:</span>
+            <span class="horizontal_list_content"> CRAFTER 30-50 Camionnette</span>
+          </li>
+          <li  class="list-group-item">
+            <span class="horizontal_list_header">Periodo:</span>
+            <span class="horizontal_list_content"> DEPUIS 04-2006</span>
+          </li>
+          <li  class="list-group-item">
+            <span class="horizontal_list_header">Motor:</span>
+            <span class="horizontal_list_content"> salamadonna ABSDFS</span>
+          </li>
+        </ul>
+      </div>
+      <div id="step3" class="tab-pane fade">
+        <ul class="list-group">
+          <li  class="list-group-item">
+            <span class="horizontal_list_header">Nombre y apellidos:</span>
+            <span class="horizontal_list_content">Poldo de poldis ramirez hernandez de la fuente torres</span>
+          </li>
+          <li  class="list-group-item">
+            <span class="horizontal_list_header">Telefono:</span>
+            <span class="horizontal_list_content">654654654</span>
+          </li>
+          <li  class="list-group-item">
+            <span class="horizontal_list_header">Email:</span>
+            <span class="horizontal_list_content">thelargestemailintheworld@superlargedomain.com </span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </div>
