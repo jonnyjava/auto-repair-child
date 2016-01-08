e7b5c9acc14311a304bc8145bb47a360d0a14618
jonnyjava <jonnyjava.net@gmail.com>
2016-01-08 08:16:07 +0100

    mail footer

--- models/123mailer.php
+++ models/123mailer.php
@@ -46,7 +46,7 @@ Class DemandMailer {
     $data = array( "to" => array($demand->email => $demand->name_and_surnames),
       "from" => array('contacto@123mecanico.es', '123mecanico'),
       "subject" => " Tu peticion de servicio en 123mecanico.es",
-      "html" =>  $this->build_html_body($demand)
+      "html" =>  $this->build_html_content($demand)
     );
     return $res = $mailin->send_email($data);
   }
@@ -60,19 +60,21 @@ Class DemandMailer {
     $mail->addReplyTo('contact@123mecanico.com', '123Mecanico');
     $mail->isHTML(true);
     $mail->Subject = ' Tu peticion de servicio en 123mecanico.es';
-    $mail->Body    = $this->build_html_body($demand);
+    $mail->Body    = $this->build_html_content($demand);
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     return $mail;
   }
 
-  private function build_html_body($demand){
+  private function build_html_content($demand){
     $text = "<table style='font-family:Calibri,Roboto,Arial;font-size:1em;padding:5px'>";
-    $text .= $this->build_intro();
+    $text .= $this->build_header();
+    $text .= $this->build_separator();
     $text .= $this->build_step1($demand);
     $text .= $this->build_separator();
     $text .= $this->build_step2($demand);
     $text .= $this->build_separator();
     $text .= $this->build_step3($demand);
+    $text .= $this->build_footer();
     $text .= "</table>";
     return $text;
   }
@@ -81,7 +83,7 @@ Class DemandMailer {
     return "<tr><td colspan='2'><hr style='border:0;border-bottom:1px dashed #FFFFFF;height:1px;line-height:1px;background:#666666;'/></td></tr>";
   }
 
-  private function build_intro(){
+  private function build_header(){
     $text = "<tr><td colspan='2' style='padding:0.5em;'>";
     $text .= "Hemos recibido tu demanda y está siendo examinada por nuestros expertos, gracias por tu confianza!<br/>";
     $text .= "Aquí te detallamos cuales serán los próximos pasos:";
@@ -92,10 +94,7 @@ Class DemandMailer {
     $text .= "<li>A recibirlos, podrás elegir el taller que prefieras, y después de haber elegido uno, deberás llamarlo para definir una fecha de intervención.</li>";
     $text .= "<li>Durante la intervención, pagaras directamente el taller por su trabajo.</li>";
     $text .= "</ol>";
-    $text .= "<p>";
-    $text .= "Si tienes alguna duda, contactanos a esta dirección: <a href='mailto:contact@123mecanico.es' style=' color: #E67500;text-decoration: none;cursor: pointer;'>contact@123mecanico.es</a>, te contestáramos lo más rápidamente posible.<br/>";
-    $text .= "También puedes visitar nuestra web la página: <a href='http://123mecanico.es/como-funciona/' target='blank' style=' color: #E67500;text-decoration: none;cursor: pointer;'>como funciona</a> para encontrar respuestas a tus dudas.";
-    $text .= "</p></td></tr>";
+    $text .= "</td></tr>";
     return $text;
   }
 
@@ -154,6 +153,14 @@ Class DemandMailer {
     return $text;
   }
 
+  private function build_footer(){
+    $text = "<tr><td colspan='2' style='padding:0.5em;'>";
+    $text .= "Si tienes alguna duda, contactanos a esta dirección: <a href='mailto:contact@123mecanico.es' style=' color: #E67500;text-decoration: none;cursor: pointer;'>contact@123mecanico.es</a>, te contestáramos lo más rápidamente posible.<br/>";
+    $text .= "También puedes visitar nuestra web la página: <a href='http://123mecanico.es/como-funciona/' target='blank' style=' color: #E67500;text-decoration: none;cursor: pointer;'>como funciona</a> para encontrar respuestas a tus dudas.";
+    $text .= "<br/><br/>El equipo de 123Mecanico.es";
+    $text .= "</td></tr>";
+    return $text;
+  }
   private function translate($value){
     $translated = $this->translations[$value] != '' ? $this->translations[$value] : $value;
     return $translated;
