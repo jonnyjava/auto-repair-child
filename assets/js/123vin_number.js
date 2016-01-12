9d87890f594c74c27bf43f89029da31ab7bfdcb1
jonnyjava <jonnyjava.net@gmail.com>
2016-01-12 11:24:06 +0100

    dropdown autofill

--- assets/js/123vin_number.js
+++ assets/js/123vin_number.js
@@ -1,14 +1,39 @@
 function activate_vin_number_search(){
   $('#vin_number_searcher').click(function(){
     var my_destination = $('#onboarding_form').attr('action');
-    //var vin_number = $('#vin_number').val().toUpperCase();
-    //var serialized_datas = 'vin_number='+vin_number;
-
-    var serialized_datas ="vin_number=WF05XXGCD56R45366";
-    $.ajax({type: 'GET', data: serialized_datas, url: my_destination, async: true}).success(function(response){
-      console.log("---> "+response);
+    var vin_number = $('#vin_number').val().toUpperCase();
+    var serialized_datas = 'vin_number='+vin_number;
+    $.ajax({type: 'POST', data: serialized_datas, url: my_destination, async: true}).success(function(response){
+      var parsed_response = jQuery.parseJSON(response);
+      if(parsed_response.status == 400){
+        console.log("algo ha ido mal con el parsing");
+      }
+      else{
+        autofill_car_details_dropdowns(parsed_response.car_details);
+      }
     }).error(function(parsed_response){
-      $('.js_detail_toggler').click();
+      console.log('kaboom');
     });
+    $('.js_detail_toggler').click();
+    show_message();
   });
+}
+
+function show_message(){
+  //console.log('ya me lo pienso');
+}
+
+function autofill_car_details_dropdowns(car_details){
+  var details = jQuery.parseJSON(car_details);
+  $.each(details, function(key, value){
+    var choosen_option = value;
+    var parentId = $('#car_'+key+'_dropdown').data('parent-id');
+    if(key != 'brand'){
+      enable_field(key, '');
+    }
+    $('#'+parentId).text(choosen_option);
+    $('#'+parentId+'_id').val(choosen_option);
+    $('#'+parentId+'_tooltip').hide();
+  });
+  enable_field('engine_letters');
 }
--- controllers/vin_controller.php
+++ controllers/vin_controller.php
@@ -1,11 +1,12 @@
 <?php
-  $vin_number = $_GET['vin_number'];
+  $vin_number = $_POST['vin_number'];
   if (!file_exists("../vin_cache/".$vin_number.".html")) {
     $vinrequest = file_get_contents("http://en.vindecoder.pl/".$vin_number);
     $myfile = fopen("../vin_cache/".$vin_number.".html", "w") or die("Unable to open file!");
     fwrite($myfile, $vinrequest);
     fclose($myfile);
   }
+//$response = json_encode(array('status' => 400, 'errors' => $demand->errorMessages));
 
   $doc = new DOMDocument();
   $doc->loadHTMLFile("../vin_cache/".$vin_number.".html");
@@ -29,5 +30,6 @@
         break;
     }
   }
-  echo json_encode($response);
+  $response = json_encode(array('status' => 200, 'car_details' =>json_encode($response) ));
+  echo $response;
 ?>
