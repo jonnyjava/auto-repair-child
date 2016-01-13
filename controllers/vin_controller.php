88f7a34e9c368651cb74217bff1463a6c5d7d34b
jonnyjava <jonnyjava.net@gmail.com>
2016-01-13 10:56:23 +0100

    vin number backend validation

--- controllers/vin_controller.php
+++ controllers/vin_controller.php
@@ -3,21 +3,28 @@ $vin_number = $_POST['vin_number'];
 echo get_car_details($vin_number);
 
 function get_car_details($vin_number){
-  if (isset($vin_number)) {
+  if ( isset($vin_number) && vin_number_is_valid($vin_number) ){
     $vin_is_cached = cache_vin_if_not_exists($vin_number);
     if ($vin_is_cached){
       $response = ok_response($vin_number);
     }
     else{
-      $response = error_response();
+      $response = error_response('error a recuperar el numero de bastidor desde el DB');
     }
   }
   else{
-    $response = error_response();
+    $response = error_response('El numero de bastidor es invalido');
   }
   return $response;
 }
 
+
+function vin_number_is_valid($vin_number){
+  $vin_is_valid = true;
+  preg_match('/[ABCDEFGHLJKMNPRSTUVXYWZ0-9]{17}$/', $vin_number, $matches);//IOQÑ are not allowed
+  $vin_is_valid = ( (strlen($vin_number) == 17) && (count($matches) > 0) );
+  return $vin_is_valid;
+}
 
 function cache_vin_if_not_exists($vin_number){
   $allok = true;
@@ -63,8 +70,9 @@ function ok_response($vin_number){
   return $response;
 }
 
-function error_response(){
+function error_response($error_description){
   $errors['server_answer'] = 'La pagina no responde, no existe o no contiene nada';
+  $errors['error_description'] = $error_description;
   $response = json_encode(array('status' => 400, 'errors' => json_encode($errors) ));
   return $response;
 }
