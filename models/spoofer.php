d94d220568764271303e7e7effe2956d940bc52d
jonnyjava <jonnyjava.net@gmail.com>
2016-02-23 12:04:11 +0100

    proxy scrapper and checker

--- models/spoofer.php
+++ models/spoofer.php
@@ -1,6 +1,12 @@
 <?php
 $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
 require_once( $parse_uri[0] . 'wp-load.php' );
+/*
+//require_once( 'ESTÉ DONDE ESTË wp-load.php' );
+require 'models/spoofer.php';
+$spoofer = new Spoofer();
+$spoofer->checkAllProxies();
+*/
 
 Class Spoofer{
 
@@ -11,6 +17,17 @@ Class Spoofer{
 
   public function get_proxy(){
     return $this->get_valid_proxy();
+  }
+
+  public function checkAllProxies(){
+    $proxies = $this->getAllProxies();
+    foreach($proxies as $proxy){
+      $ip = $proxy['ip'];
+      $proxy_is_up = $this->proxy_is_available($ip);
+      if (!$proxy_is_up){
+        $this->increment_proxy_down_counter($proxy);
+      }
+    }
   }
 
   private function get_valid_proxy(){
@@ -25,16 +42,22 @@ Class Spoofer{
   }
 
   private function get_proxy_from_db(){
-    $query = "select * from wp_proxies order by rand() limit 1";
+    $query = "SELECT ip FROM wp_proxies WHERE times_down = (SELECT MIN(times_down) FROM wp_proxies) ORDER BY rand() limit 1";
     $proxy = $this->db->get_row($query, ARRAY_A);
     return $proxy;
+  }
+
+  private function getAllProxies(){
+    $query = "SELECT ip FROM wp_proxies ORDER BY times_down";
+    $proxies = $this->db->get_results($query, ARRAY_A);
+    return $proxies;
   }
 
   private function proxy_is_available($proxy){
     $ip = explode(":", $proxy)[0];
     $port = explode(":", $proxy)[1];
     $proxy_is_up = true;
-    $socket = fSockOpen($ip, $port, $errno, $errstr, 10);
+    $socket = fSockOpen($ip, $port, $errno, $errstr, 5);
     if (!$socket) {
       $proxy_is_up = false;
     }
